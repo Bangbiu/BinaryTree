8c50d9c57b7f43a0d5a6a27f03f415f77fad4afc
Empty <Empty_hundred@outlook.com>
2019-07-03 07:56:29 +0800

    update node

--- Node.java
+++ Node.java
@@ -3,5 +3,23 @@ public class Node{
 	Node leftNode;
 	Node rightNode;
 	String name;
+	public Node(){
+	
+	}
+	public Node(Node parent)
+	{
+		this.parent=parent;
+	}
+	public Node(Node left,Node right)
+	{
+		this.leftNode=left;
+		this.rightNode=right;
+	}
+	public Node(Node parent,Node left,Node right)
+	{
+		this.leftNode=left;
+		this.rightNode=right;
+		this.parent=parent;
+	}
 	//hashcode;
 }
