762452c613b05fa9134a68730eb62cab2db9e1ac
Empty <Empty_hundred@outlook.com>
2019-07-04 16:15:19 +0800

    Update

--- BinTree.java
+++ BinTree.java
@@ -1,12 +1,30 @@
 public class BinTree{
+	public String name;
 	public Node root;
+	
+	public BinTree()
+	{
+		name="";
+	}
+	
+	public BinTree(String name,Node root)
+	{
+		this.name=name;
+		this.root=root;
+	}
+	
+	public BinTree(String name)
+	{
+		this.name=name;
+	}
 	public BinTree buildTree(Node root,String DLR,String LDR,String LRD)
 	{
 		
-		return buildTree(root,DLR,LDR,LRD,",");
+		return buildTree(root,DLR,LDR,LRD,',');
 	}
 	public BinTree buildTree(Node root,String DLR,String LDR,String LRD,char Separator)
 	{
+		/*
 		if(DLR!=null && LDR!=null)
 			return buildByFM(root,DLR,LDR,Separator);
 		else if(DLR!=null && LDR!=null)
@@ -15,8 +33,10 @@ public class BinTree{
 			return buildByMR(root,LDR,LRD,Separator);
 		else
 			return null;
-		
+		*/
+		return null;
 	}
+	/*
 	private BinTree buildByFM(Node root,String DLR,String LDR,char Separator)
 	{
 	
@@ -31,26 +51,102 @@ public class BinTree{
 	{
 	
 	}
+	*/
+
 	
+	//DLR
 	public String getDLR(){
-		
+		return getDLR(this.root);
 	}
 	
+	public String getDLR(Node root){
+		return getDLR(root,',');
+	}
+	public String getDLR(Node root,char Separator){
+		if(root==null)return "";
+		return root.name+ addSepL(getDLR(root.leftNode,Separator),Separator)+ addSepL(getDLR(root.rightNode,Separator),Separator);
+	}
+	
+	//LDR
 	public String getLDR(){
-		
+		return getLDR(this.root);
 	}
 	
+	public String getLDR(Node root){
+		return getLDR(root,',');
+	}
+	
+	public String getLDR(Node root,char Separator){
+		if(root==null)return "";
+		return addSepR(getLDR(root.leftNode,Separator),Separator)+root.name +addSepL(getLDR(root.rightNode,Separator),Separator);
+	}
+	
+	//LRD
 	public String getLRD(){
-		
+		return getLRD(this.root);
 	}
 	
-	public Node findNode(String name)
-	{
-		
+	public String getLRD(Node root){
+		return getLRD(root,',');
 	}
 	
-	public Node getLayer(Node node)
-	{
-		
+	public String getLRD(Node root,char Separator){
+		if(root==null)return "";
+		return addSepR(getLRD(root.leftNode,Separator),Separator) + addSepR(getLRD(root.rightNode,Separator),Separator)+root.name;
 	}
+	
+	//findNode
+	public Node getNode(String name)
+	{
+		return getNode(this.root,name);
+	}
+	
+	public Node getNode(Node root,String name)
+	{
+		//Self
+		if(root==null)return null;
+		if(root.name.equals(name))return root;
+		//L Tree
+		Node Lres=getNode(root.leftNode,name);
+		if(Lres!=null)return Lres;
+		//R Tree
+		Node Rres=getNode(root.rightNode,name);
+		return Rres;
+	}
+	
+	//Depth
+	public int getDepth(){
+		return getDepth(this.root);
+	}
+	
+	public int getDepth(BinTree tree)
+	{
+		return getDepth(tree.root);
+	}
+	
+	public int getDepth(Node root)
+	{
+		if(root==null)return 0;
+		int layL=getDepth(root.leftNode);
+		int layR=getDepth(root.rightNode);
+		if(layL>layR)
+			return layL+1;
+		else
+			return layR+1;
+	}
+	
+	private String addSepL(String oriStr,char Separator){
+		if(oriStr!="")
+			return  Separator+oriStr;
+		else
+			return "";
+	}
+	private String addSepR(String oriStr,char Separator){
+		if(oriStr!="")
+			return oriStr+Separator;
+		else
+			return "";
+	}
+	
+
 }
--- BinTreeTest.java
+++ BinTreeTest.java
@@ -1,7 +1,42 @@
+
+import java.awt.*;
 public class BinTreeTest{
 	public static void main(String args[]){
 		BinTree a=new BinTree();
-		a.root=new Node("f",null,new Node("k",null,new Node("a"),new Node("b")),new Node("c"));
-		
+		BinTree b;
+		a.root=new Node("f",null,new Node("k",null,new Node("a",null,new Node("o"),new Node("l")),new Node("b")),new Node("c",null,new Node("g"),null));
+		b=new BinTree("myTree",
+		new Node("1",
+			new Node("2",
+				new Node("4",
+					new Node("7"),
+					new Node("8",
+						null,
+						new Node("10",
+							new Node("12"),
+							null
+						)
+					)
+				),
+				new Node("5")
+			),
+			new Node("3",
+				null,
+				new Node("6",
+					new Node("9",
+						null,
+						new Node("11")
+					),
+					null
+				)
+			)
+		)
+		);
+		System.out.println("DLR: " + b.getDLR());
+		System.out.println("LDR: " + b.getLDR());
+		System.out.println("LRD: " + b.getLRD());
+		System.out.println(a.getDepth());
+		TreeDisplayer Disp = new TreeDisplayer(b,500);
+		//System.out.println(a.getNode("k").name);
 	}
 }
